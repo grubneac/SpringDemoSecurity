83668f4e6ef84e2c057c04566e80866c2a7bbcaf
Grubneac <grubneac@yandex.ru>
2021-01-30 20:49:03 +0200

    Initial commit

---
+++ src/main/java/com/grubneac/springsecuritydemo/controller/DeveloperRestControllerV1.java
@@ -0,0 +1,34 @@
+package com.grubneac.springsecuritydemo.controller;
+
+import com.grubneac.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Serghei", "Sergheev"),
+            new Developer(3L, "Petr", "Petrov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
