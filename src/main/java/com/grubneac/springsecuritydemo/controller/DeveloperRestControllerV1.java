4fbbaeebe4ed3b0d2f07e595288db8de57b57276
Grubneac <grubneac@yandex.ru>
2021-01-31 11:37:44 +0200

    add annotation @PreAuthorize in REST controller
    and remove from config permissions

--- src/main/java/com/grubneac/springsecuritydemo/controller/DeveloperRestControllerV1.java
+++ src/main/java/com/grubneac/springsecuritydemo/controller/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package com.grubneac.springsecuritydemo.controller;
 
 import com.grubneac.springsecuritydemo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
@@ -30,12 +32,14 @@ public class DeveloperRestControllerV1 {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer) {
         DEVELOPERS.add(developer);
         return developer;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable Long id) {
         DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
