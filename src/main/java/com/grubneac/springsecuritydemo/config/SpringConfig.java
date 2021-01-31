c5e9a80b36311d554dc206a90b94c591ef096f0f
Grubneac <grubneac@yandex.ru>
2021-01-31 11:04:41 +0200

    add permissions and for each user change role to authorities

--- src/main/java/com/grubneac/springsecuritydemo/config/SpringConfig.java
+++ src/main/java/com/grubneac/springsecuritydemo/config/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.grubneac.springsecuritydemo.config;
 
+import com.grubneac.springsecuritydemo.model.Permission;
 import com.grubneac.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,9 +24,9 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(),Role.USER.name())
-                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET,"/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                .antMatchers(HttpMethod.POST,"/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -41,12 +42,12 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User.builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
